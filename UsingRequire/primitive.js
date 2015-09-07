68d0476d7b6d097a86cbdfddd01e62c0c98d554b
Ashutosh Prakash <prakash.ashutosh12@gmail.com>
2015-09-08 03:20:18 +0530

    acts as a class
    has 3 functions..acts as a class basically

---
+++ UsingRequire/primitive.js
@@ -0,0 +1,20 @@
+module.exports = {
+	diff :function(a,b){
+		return a-b;
+	},
+	sum : function (a,b){
+		return a+b;
+	},
+	sqrt: function (a,b){
+		return a+b;
+	}
+}
+//either
+
+exports.area = function (r) {
+  return PI * r * r;
+};
+
+exports.circumference = function (r) {
+  return 2 * PI * r;
+};
